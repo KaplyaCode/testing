c1bf5923d3f0f3b044ca0b8895ca4eed92991a85
KaplyaKode <illyakapyshin@gmail.com>
2021-10-27 18:30:17 +0300

    Create Result Page

---
+++ TestingStudentKnowledge/Results.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingStudentKnowledge
+{
+    public class Results
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Faculty { get; set; }
+        public int Points { get; set; }
+    }
+}
